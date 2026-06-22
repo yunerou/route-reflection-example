9a60acfee932e1401311086cb26c40c227df7fe0
yunerou <yunerou yunerou@gmail.com>
2026-06-22 16:40:42 +0700

    f

--- app/server/config-router.go
+++ app/server/config-router.go
@@ -15,8 +15,8 @@ import (
 type ErrorResponse struct {
 	status  int
 	Message string `json:"message" doc:"Mô tả lỗi"`
-	Code    string `json:"code" doc:"Mã lỗi nội bộ" example:"INTERNAL_FAILURE"`
-	Details any    `json:"details,omitempty" doc:"Chi tiết bổ sung"`
+	Code    string `json:"code" doc:"Code lỗi nội bộ" example:"INTERNAL_FAILURE"`
+	Details any    `json:"details,omitempty" doc:"Chi tiết bổ sung, chỉ có trên một số Code nhất định"`
 }
 
 func (e *ErrorResponse) Error() string  { return e.Message }
